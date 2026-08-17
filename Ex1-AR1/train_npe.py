9c49c8f78cc82b66eb5f82fdf795eab00a30b26e
gyanstats <112892065+gyanstats@users.noreply.github.com>
2026-08-17 18:38:12 +0100

    Update train_npe.py

--- Ex1-AR1/train_npe.py
+++ Ex1-AR1/train_npe.py
@@ -120,6 +120,8 @@ if train_multiple:
     filename = f'results_ncle/npe_N{N_string(num_sims)}/train_l{batch_len}_{task_id}.pkl'
 else:
     filename = f'results_ncle/npe_N{N_string(num_sims)}/train_l{batch_len}.pkl'
+
+os.makedirs(os.path.dirname(filename), exist_ok=True)
 with open(filename, 'wb') as f:
     pickle.dump(training_results, f)
     
